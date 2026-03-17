e86abbc4b0fd9d64ec24d00d1075c4960a9d080b
tprichsmart-ops <tp.richsmart@gmail.com>
2026-03-17 11:07:53 +0800

    Update server.js

--- server.js
+++ server.js
@@ -22,15 +22,21 @@ app.post('/webhook', line.middleware(config), (req, res) => {
 
 async function handleEvent(event) {
 
-  if (event.type === 'message' && event.message.type === 'text') {
+  if (event.type !== 'message') {
+    return Promise.resolve(null)
+  }
+
+  if (event.message.type === 'text') {
 
     if (event.message.text === '我來為2026全障運選手加油了~') {
 
       const messages = [
+
         {
           type: 'text',
           text: '嗨～我是小編 😊\n歡迎加入我們的 Line！\n\n今年我們很榮幸參與\n2026 全障運賽事的贊助與支持\n\n一起為努力突破自我的選手們加油 💪'
         },
+
         {
           type: 'template',
           altText: '領取優惠券',
@@ -38,75 +44,33 @@ async function handleEvent(event) {
             type: 'buttons',
             text: '為了替選手們應援\n小編準備了一份 Epson M310 專屬優惠券',
             actions: [
+
               {
-                type: 'postback',
+                type: 'uri',
                 label: '領取 M310 優惠券',
-                data: 'action=pg2026_coupon'
+                uri: 'https://lin.ee/nP7OLzc'
+              },
+
+              {
+                type: 'uri',
+                label: '查看 M310 型錄',
+                uri: 'https://myppt.cc/TWD9lK'
               }
+
             ]
           }
         }
+
       ]
 
       return client.replyMessage(event.replyToken, messages)
-    }
-  }
 
-  if (event.type === 'postback') {
-
-  if (event.postback.data === 'action=pg2026_coupon') {
-
-  const messages = [
-
-    {
-      type: 'text',
-      text: '小編已幫您準備好 Epson M310 專屬優惠券 🎁'
-    },
-
-    {
-      type: 'template',
-      altText: '開啟優惠券',
-      template: {
-        type: 'buttons',
-        text: '點擊下方按鈕開啟優惠券',
-        actions: [
-          {
-            type: 'uri',
-            label: '開啟 M310 優惠券',
-            uri: 'https://line.me/R/ch/1654883387?couponId=01KKWNX2DTRQ8G3S9X8YE0E77X'
-          }
-        ]
-      }
-    },
-
-    {
-      type: 'template',
-      altText: '下載型錄',
-      template: {
-        type: 'buttons',
-        text: '需要產品詳細資訊可以下載型錄',
-        actions: [
-          {
-            type: 'uri',
-            label: '下載 M310 型錄',
-            uri: 'https://myppt.cc/TWD9lK'
-          }
-        ]
-      }
-    },
-
-    {
-      type: 'text',
-      text: '想先認識一下新朋友 😊\n\n請跟小編分享：\n1️⃣ 服務單位\n2️⃣ 您的大名'
     }
 
-  ]
-
-  return client.replyMessage(event.replyToken, messages)
-}
   }
 
   return Promise.resolve(null)
+
 }
 
 const port = process.env.PORT || 3000
