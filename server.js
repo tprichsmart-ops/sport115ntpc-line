fba19c4a61c3f755ded25c6aeb5396dc24a72026
tprichsmart-ops <tp.richsmart@gmail.com>
2026-03-17 12:22:50 +0800

    Update server.js

--- server.js
+++ server.js
@@ -50,27 +50,73 @@ async function handleEvent(event) {
             '一起為努力突破自我的選手們加油 💪'
         },
         {
-          type: 'template',
+          type: 'flex',
           altText: 'M310 優惠券與型錄',
-          template: {
-            type: 'buttons',
-            text:
-              '為了替選手們應援\n' +
-              '小編準備了一份 Epson M310 專屬優惠券\n' +
-              '有需要的朋友可以直接領取 👇',
-            actions: [
-              {
-                type: 'uri',
-                label: '領取 M310 優惠券',
-                uri: 'https://lin.ee/nP7OLzc'
-              },
-              {
-                type: 'postback',
-                label: '查看 M310 型錄',
-                data: 'action=view_catalog'
-              }
-            ]
+          contents: {
+            type: 'bubble',
+            body: {
+              type: 'box',
+              layout: 'vertical',
+              spacing: 'md',
+              contents: [
+                {
+                  type: 'text',
+                  text: '2026 全障運專屬應援禮',
+                  weight: 'bold',
+                  size: 'xl',
+                  wrap: true,
+                  color: '#111111'
+                },
+                {
+                  type: 'text',
+                  text: '小編已幫您準備好 Epson M310 專屬優惠券與型錄，有需要可直接查看。',
+                  size: 'sm',
+                  color: '#666666',
+                  wrap: true
+                },
+                {
+                  type: 'separator',
+                  margin: 'md'
+                },
+                {
+                  type: 'box',
+                  layout: 'vertical',
+                  spacing: 'sm',
+                  margin: 'md',
+                  contents: [
+                    {
+                      type: 'button',
+                      style: 'primary',
+                      height: 'sm',
+                      action: {
+                        type: 'uri',
+                        label: '領取 M310 優惠券',
+                        uri: 'https://lin.ee/nP7OLzc'
+                      }
+                    },
+                    {
+                      type: 'button',
+                      style: 'secondary',
+                      height: 'sm',
+                      action: {
+                        type: 'postback',
+                        label: '查看 M310 型錄',
+                        data: 'action=view_catalog'
+                      }
+                    }
+                  ]
+                }
+              ]
+            }
           }
+        },
+        {
+          type: 'text',
+          text:
+            '如果未來需要設備或相關資訊，\n' +
+            '我會第一時間提供給您。\n' +
+            '先讓我認識一下新朋友 😊\n\n' +
+            '趕快留下您的服務單位與大名吧~'
         }
       ];
 
@@ -80,7 +126,7 @@ async function handleEvent(event) {
     return null;
   }
 
-  // postback：查看型錄
+  // postback：查看型錄，送出兩張圖
   if (event.type === 'postback') {
     if (event.postback.data === 'action=view_catalog') {
       const messages = [
